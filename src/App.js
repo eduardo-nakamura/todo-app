9f3e7a12c32db5ed93b5fe7f74dcb5ba4683a6e8
eduardo-nakamura <edu-nakamura@hotmail.com>
2020-08-29 13:27:26 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './App.css';
 import Form from "./components/Form"
 import TodoList from "./components/TodoList"
 function App() {
-  const [inputText, setInputText] = useState("")
-  const [todos, setTodos] = useState([])
+  const [inputText, setInputText] = useState("")  
+  const [todos, setTodos] = useState([])//todos  
+  const [status,setStatus] = useState("all")//Status filtro
+  const [filterTodos, setFilterTodos] = useState([])//filtro
+
+  // on init
+  useEffect(() => {    
+    getLocalTodos()
+  },[])
+
+  // quando usa filtro
+  useEffect(() => {
+    filterHandler()
+    saveLocalTodos()
+  },[todos, status])
+
+  const filterHandler = () =>{
+    switch(status){
+      case 'completed':
+        setFilterTodos(todos.filter(todo => todo.completed === true))
+        break;
+      case 'uncompleted':
+        setFilterTodos(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilterTodos(todos)
+    }
+  }
+  // save localsotrage
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]))
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'))
+      setTodos(todoLocal)
+    }
+  }
 
   return (
     <div className="App">
       <header>
         <h1>Ed's Todo List</h1>
       </header>
-      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
-      <TodoList  />
-      <a href="https://youtu.be/pCA4qpQDZD8?list=WL&t=1951" target="_blank">TodoList Tutorial</a>
+      <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+        
+      />
+      <TodoList setTodos={setTodos} todos={todos} filterTodos={filterTodos} />
+      <a href="https://youtu.be/pCA4qpQDZD8?list=WL&t=1951" >TodoList Tutorial</a>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
-const TodoList = () =>{
+import Todo from './Todo'
+const TodoList = ({ todos,setTodos,filterTodos }) =>{
+    console.log(filterTodos)
+    console.log(todos)
     return (
         <div className="todo-container">
             <ul className="todo-list">
-                
+                {filterTodos.map(todo => (
+                    <Todo setTodos={setTodos} todos={todos}  key={todo.id} todo={todo} text={todo.text}/>
+                ))}
             </ul>
+        
         </div>
     );
 }
