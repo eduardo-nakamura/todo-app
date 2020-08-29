522684f39bf79460f81b7e6c807363772362010f
eduardo-nakamura <edu-nakamura@hotmail.com>
2020-08-29 13:48:34 -0300

    Updates

---
+++ precache-manifest.606ac475a0ae4ddf26130581695d0ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78309f429b871bae83e48dd90d8e437",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "bbedb3a3e3b11ca2b7e9",
+    "url": "/todo-app/static/css/main.ebddfea1.chunk.css"
+  },
+  {
+    "revision": "b7b3c787e0542a80a5f8",
+    "url": "/todo-app/static/js/2.ab8f88eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.ab8f88eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbedb3a3e3b11ca2b7e9",
+    "url": "/todo-app/static/js/main.1c5f05b7.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
