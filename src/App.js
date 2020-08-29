72452d27e0b059df10b797930ff2c93ffb86ce61
eduardo-nakamura <edu-nakamura@hotmail.com>
2020-08-29 01:10:56 -0300

    add otod

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
 
+import './App.css';
+import Form from "./components/Form"
+import TodoList from "./components/TodoList"
 function App() {
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
+
   return (
     <div className="App">
-      <h1>Hello React</h1>   
+      <header>
+        <h1>Ed's Todo List</h1>
+      </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList  />
+      <a href="https://youtu.be/pCA4qpQDZD8?list=WL&t=1951" target="_blank">TodoList Tutorial</a>
     </div>
   );
 }
